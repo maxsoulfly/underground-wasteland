1333157f7b68cfe92e8b748ca052c0dd8349d216
maxsoulfly <max.kibinimat@gmail.com>
2025-04-06 16:06:54 +0300

    feat: initial project setup with Caminar template and PHP includes

---
+++ caminar/includes/footer.php
@@ -0,0 +1,13 @@
+<footer id="footer">
+    <div class="container">
+        <ul class="icons">
+            <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+            <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+            <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+            <li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+        </ul>
+    </div>
+</footer>
+<div class="copyright">
+    Site made with <a href="https://templated.co/">TEMPLATED.CO</a>
+</div>
